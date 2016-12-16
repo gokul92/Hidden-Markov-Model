f6c1ea531ed62a21bc8fe71e6dcd25090e6aa1c4
gokul92 <gokul.er92@gmail.com>
2016-12-16 16:58:03 +0530

    1. Typos in word emission. Changed emmis_prob -> emis_prob

--- hmm.py
+++ hmm.py
@@ -85,7 +85,8 @@ class get_data_id(object):
     
     def get_head(self):
         return self.csv_head
-        
+
+# Given open, high, low and close prices return a state for a given configuration of prices
 def ohlc_classify(op, hi, lo, cl):
     
     if hi < op:
@@ -111,7 +112,7 @@ def ohlc_classify(op, hi, lo, cl):
             else:
                 return 8
         
-fn = "C:/Users/Gokul/Desktop/NIFTY/Intraday/india/NIFTY 50.csv"
+fn = "/Users/gokul/Desktop/Finance/NIFTY/Intraday/india/NIFTY 50.csv"
 nifty_id = get_data_id(fn, 0)
 
 datelist = nifty_id.get_datelist()
@@ -147,7 +148,7 @@ for i in range(n_states):
         A[i,j] = np.random.uniform(low=0.001, high=1.0)
     A[i,:] = A[i,:]/np.sum(A[i,:])
     
-# Emmission probability matrix B
+# Emission probability matrix B
 B = np.empty(shape=(n_xstates, n_states))
 for i in range(n_xstates):
     for k in range(n_states):
@@ -168,6 +169,7 @@ iter_total = 10
 iter_no = 1
 
 while iter_no <= iter_total:
+
 #********************************EXPECTATION STEP*****************************#
 
     # Calculating alpha_base, cn[0] and base for alpha_sc
@@ -179,8 +181,8 @@ while iter_no <= iter_total:
     for i in range(1, n_data):
         for j in range(n_states):
             loc = np.where(x[i] == x_states)[0]
-            emmis_prob = B[loc, j]
-            delta[i,j] = emmis_prob*np.sum(alpha_sc[i-1,:]*A[:,j])
+            emis_prob = B[loc, j]
+            delta[i,j] = emis_prob*np.sum(alpha_sc[i-1,:]*A[:,j])
         cn[i] = np.sum(delta[i,:])
         alpha_sc[i,:] = delta[i,:]/cn[i]
     
@@ -191,8 +193,8 @@ while iter_no <= iter_total:
     for i in range(n_data-2, -1, -1):
         for j in range(n_states):
             loc = np.where(x[i+1] == x_states)[0]
-            emmis_prob = B[loc, j]
-            epsilon[i,j] = np.sum(beta_sc[i+1,:]*emmis_prob*A[j,:])
+            emis_prob = B[loc, j]
+            epsilon[i,j] = np.sum(beta_sc[i+1,:]*emis_prob*A[j,:])
         beta_sc[i,:] = epsilon[i,:]/cn[i+1]
     
 #********************************MAXIMIZATION STEP****************************#
@@ -208,20 +210,20 @@ while iter_no <= iter_total:
             denominator = 0
             for n in range(0, n_data-1):
                 loc = np.where(x[n] == x_states)[0]
-                emmis_prob = B[loc,i]
-                numerator = numerator + alpha_sc[n-1,i]*beta_sc[n,j]*emmis_prob*A[i,j]/cn[n]
+                emis_prob = B[loc,i]
+                numerator = numerator + alpha_sc[n-1,i]*beta_sc[n,j]*emis_prob*A[i,j]/cn[n]
             for l in range(n_states): 
                 temp = 0
                 for n in range(0, n_data-1):
                     loc = np.where(x[n] == x_states)[0]
-                    emmis_prob = B[loc,l]
-                    temp = temp + alpha_sc[n-1,i]*beta_sc[n,l]*emmis_prob*A[i,l]/cn[n]
+                    emis_prob = B[loc,l]
+                    temp = temp + alpha_sc[n-1,i]*beta_sc[n,l]*emis_prob*A[i,l]/cn[n]
                 denominator = denominator + temp
             Atemp[i,j] = numerator/denominator
     
     A[:] = Atemp
     
-    # Emmission probability matrix B
+    # Emission probability matrix B
     for i in range(n_xstates):
         for j in range(n_states):
             arg_list = np.where(x == x_states[i])[0]
