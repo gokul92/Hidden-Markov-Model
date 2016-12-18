6821e8e64d14973a1f5f0f9ce8dd12b1b7748fbc
gokul92 <gokul.er92@gmail.com>
2016-12-18 18:30:49 +0530

    ohlc classifier function corrected

--- get_states.py
+++ get_states.py
@@ -1,24 +1,6 @@
 # Given open, high, low and close prices return a state for a given configuration of prices
 def ohlc_classify(op, hi, lo, cl):
-    if hi < op:
-        if lo < op:
-            if cl < op:
-                return 1
-            else:
-                return 2
-        else:
-            if cl > op:
-                return 3
-            else:
-                return 4
-    else:
-        if lo > op:
-            if cl > op:
-                return 5
-            else:
-                return 6
-        else:
-            if lo < op:
-                return 7
-            else:
-                return 8
+    if cl <= op:
+        return 7
+    elif cl > op:
+        return 8
